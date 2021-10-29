f0bd53156e62058fa1759eeccdf677b694c004da
Kiley <42876297+hewittk@users.noreply.github.com>
2021-10-29 17:48:47 -0400

    Add numbers to output returned

--- sum.py
+++ sum.py
@@ -4,5 +4,7 @@ import hug
 @hug.get()
 def sum(num1:hug.types.number=3, num2:hug.types.number=5):
     return {
-        'sum': num1 + num2
+        'First number': num1,
+        'Second number': num2,
+        'Sum': num1 + num2
     }
